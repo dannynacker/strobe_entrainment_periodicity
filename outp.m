7437a4d767bf3113f7c6e810316030f79a2d2cd6
Danny Nacker <dannynacker@yahoo.com>
2024-07-09 18:52:24 +0100

    Add files via upload

---
+++ outp.m
@@ -0,0 +1,45 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+function outp(address,byte)
+
+global cogent;
+
+%test for correct number of input arguments
+if(nargin ~= 2)
+    error('usage: outp(address,data)');
+end
+
+io64(cogent.io.ioObj,address,byte);
+
+
+}
+/*
+     FILE ARCHIVED ON 06:41:35 Sep 22, 2020 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 17:32:05 May 02, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.814
+  exclusion.robots: 0.091
+  exclusion.robots.policy: 0.078
+  cdx.remote: 0.071
+  esindex: 0.009
+  LoadShardBlock: 70.082 (3)
+  PetaboxLoader3.datanode: 111.959 (4)
+  PetaboxLoader3.resolve: 111.944 (2)
+  load_resource: 158.584
+*/
